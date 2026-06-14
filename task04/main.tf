4ad4431c1ee4e3cad0e7891991b1b478ac295553
arayiksahakyan <arayiksahakyan59@gmail.com>
2026-06-14 21:16:02 +0400

    Remove Terraform state and fix generated names

--- task04/main.tf
+++ task04/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_network_interface" "nic" {
   tags                = local.tags
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "${var.nic_name}-ipconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
